bb5eefe2ddc4785c80820349cacb2a8bf615c690
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2024-05-18 14:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	new Car(make, model);
+	this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype.getTopSpeed = function () {
+	return this.topSpeed;
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
